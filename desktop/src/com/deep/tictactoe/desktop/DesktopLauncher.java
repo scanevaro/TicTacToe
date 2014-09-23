9f54b97747d6eff6c4aad9f432849340901e19da
Sebastián <scanevaro@hotmail.com>
2014-09-23 17:00:30 -0300

    Initial commit

---
+++ desktop/src/com/deep/tictactoe/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.deep.tictactoe.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.deep.tictactoe.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
---
+++ html/src/com/deep/tictactoe/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.deep.tictactoe.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.deep.tictactoe.Main;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Main();
+        }
+}
